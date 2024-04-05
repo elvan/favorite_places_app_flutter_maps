56c5c14fd995296b6a8869dd6db944a8229c82c4
Elvan Hidayat <elvanhd@gmail.com>
2024-04-06 02:44:42 +0700

    Integrate Google Maps for location selection and display
    - Add MapScreen for displaying and selecting locations
    - Utilize google_maps_flutter package for map functionality
    - Enable navigating to MapScreen from PlaceDetailScreen
    - Configure initial map location and marker based on place details
    - Allow users to view but not edit location in PlaceDetailScreen
    - Include google_maps_flutter in pubspec.yaml for map support

--- lib/screens/place_detail.dart
+++ lib/screens/place_detail.dart
@@ -1,4 +1,5 @@
 import 'package:favorite_places/models/place.dart';
+import 'package:favorite_places/screens/map.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -33,9 +34,21 @@ class PlaceDetailScreen extends StatelessWidget {
             right: 0,
             child: Column(
               children: [
-                CircleAvatar(
-                  radius: 70,
-                  backgroundImage: NetworkImage(locationImage),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (ctx) => MapScreen(
+                          location: place.location,
+                          isSelecting: false,
+                        ),
+                      ),
+                    );
+                  },
+                  child: CircleAvatar(
+                    radius: 70,
+                    backgroundImage: NetworkImage(locationImage),
+                  ),
                 ),
                 Container(
                   alignment: Alignment.center,
