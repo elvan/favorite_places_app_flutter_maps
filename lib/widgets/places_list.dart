527c010d3e0710f8249fa60f47bc2de758a6fcdf
Elvan Hidayat <elvanhd@gmail.com>
2024-04-05 23:04:25 +0700

    Add PlaceDetailScreen and navigation
    - Create PlaceDetailScreen for displaying place details
    - Pass Place object to PlaceDetailScreen for dynamic content
    - Implement onTap navigation from PlacesList to detail view
    - Use MaterialPageRoute for navigating to PlaceDetailScreen
    - Display place title in both AppBar and body of detail view

--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -1,4 +1,5 @@
 import 'package:favorite_places/models/place.dart';
+import 'package:favorite_places/screens/place_detail.dart';
 import 'package:flutter/material.dart';
 
 class PlacesList extends StatelessWidget {
@@ -28,6 +29,13 @@ class PlacesList extends StatelessWidget {
                 color: Theme.of(context).colorScheme.onBackground,
               ),
         ),
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (ctx) => PlaceDetailScreen(place: places[index]),
+            ),
+          );
+        },
       ),
     );
   }
