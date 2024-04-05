6cb13f6cdd4b6aaafb338d8868e807c12331b5d7
Elvan Hidayat <elvanhd@gmail.com>
2024-04-05 23:28:34 +0700

    Add image capture functionality in AddPlace
    - Introduce ImageInput widget for capturing pictures
    - Add image_picker to pubspec.yaml for camera access
    - Implement camera capture in ImageInput with ImagePicker
    - Display captured image or 'Take Picture' button in UI
    - Incorporate ImageInput widget in AddPlaceScreen layout
    - Adjust layout to accommodate ImageInput with proper spacing

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,4 +1,5 @@
 import 'package:favorite_places/providers/user_places.dart';
+import 'package:favorite_places/widgets/image_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -49,6 +50,8 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
                 color: Theme.of(context).colorScheme.onBackground,
               ),
             ),
+            const SizedBox(height: 10),
+            const ImageInput(),
             const SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: _savePlace,
