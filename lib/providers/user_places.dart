e9a7a2d66a30dbf99866148a7e08a0504160f3dc
Elvan Hidayat <elvanhd@gmail.com>
2024-04-06 04:30:13 +0700

    Enhance image storage and add database support
    - Implement image file copying to app directory for persistence
    - Use path and path_provider packages for file management
    - Prepare for database integration with sqflite package inclusion
    - Update UserPlacesNotifier to store copied image file path

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -2,12 +2,19 @@ import 'dart:io';
 
 import 'package:favorite_places/models/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
-  void addPlace(String title, File image, PlaceLocation location) {
-    final newPlace = Place(title: title, image: image, location: location);
+  void addPlace(String title, File image, PlaceLocation location) async {
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final filename = path.basename(image.path);
+    final copiedImage = await image.copy('${appDir.path}/$filename');
+
+    final newPlace =
+        Place(title: title, image: copiedImage, location: location);
     state = [newPlace, ...state];
   }
 }
